efe9ef55286119fcc1309efc4616cdbaaafb6108
Khadim <khadim.bousso@gmail.com>
2021-07-22 10:30:25 +0200

    premiere ebauche bio

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProjetRepository;
+use App\Repository\BiographieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,10 +16,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="_accueil")
      */
-    public function index(ProjetRepository $projetRepository): Response
+    public function index(ProjetRepository $projetRepository, BiographieRepository $biographieRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'projets' => $projetRepository->findAll(),
+            'biographies' => $biographieRepository->findAll(),
             ]);
     }
 }
